bb6e7b0bc74eba3743b3bfa9d129d8e7d0bf746c
GrahamJReid <grahamreidtechnology@gmail.com>
2022-12-12 22:53:24 -0600

    edit items finalization1

--- pages/createItemPage.js
+++ pages/createItemPage.js
@@ -4,11 +4,11 @@ const createItemPage = (obj = {}) => {
   const domString = `<form id="${obj.firebaseKey ? `update-Item--${obj.firebaseKey}` : 'submit-Item'}">
   <div class="form-group">
     <label for="exampleInputEmail1">Name Of Item</label>
-    <input type="text" class="form-control" id="item_name" aria-describedby="emailHelp" placeholder="">
+    <input type="text" class="form-control" id="item_name" aria-describedby="emailHelp" placeholder="" value="${obj.name || ''}">
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Price</label>
-    <input type="text" class="form-control" id="price" placeholder="">
+    <input type="text" class="form-control" id="price" placeholder="" value="${obj.price || ''}">
   </div>
   <button type="submit" class="btn btn-primary">Add/Edit</button>
 </form>`;
