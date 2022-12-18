89ffa795289b4388b5d8cf8449a57d6e96ba4261
Angie Gonzalez <angiem.go@gmail.com>
2022-12-17 18:24:36 -0600

    calendar for date range on dom

--- utils/datePicker.js
+++ utils/datePicker.js
@@ -4,6 +4,12 @@ const datePick = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
     if (e.target.id.includes('dateButton')) {
       console.warn('CLICKED the submit date range button', e.target.id);
+      const startDate = document.querySelector('#startDate').value;
+      const editStartDate = new Date(startDate).toLocaleDateString();
+      const endDate = document.querySelector('#endDate').value;
+      console.warn('startDate', startDate);
+      console.warn('editStartDate', editStartDate);
+      console.warn('endDate', endDate);
     }
   });
 };
