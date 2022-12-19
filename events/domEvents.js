af9f73a6e6c610eef1196d31a81242f4435228fb
Wesley Vance <wslyvnce@gmail.com>
2022-12-19 06:06:09 -0700

    testing filter by open orders

--- events/domEvents.js
+++ events/domEvents.js
@@ -16,6 +16,7 @@ import getOrderByDeletedItem from '../utils/getOrderIdByDeletedItem';
 import { createSearchOrderArray } from '../utils/createArray/createSearchOrderArray';
 import { createSearchItemArray } from '../utils/createArray/createSearchItemArray';
 import clearView from '../utils/clear/clearView';
+import renderToDOM from '../utils/renderToDOM';
 
 const domEvents = (user) => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
@@ -71,11 +72,29 @@ const domEvents = (user) => {
     if (e.target.id.includes('openOrders')) {
       getOrder(user.uid).then((arr) => {
         let domStringFilter = '';
-        arr.forEach((order) => 
-        if (order.is_open===true) {
-          domStringFilter += ``
+        arr.forEach((item) => {
+          if (item.is_open === true) {
+            domStringFilter += `<div class="card">
+    
+          <div class="card-body" style="height: 350px;">
+            <h5 class="card-title">Order: ${item.order_name}</h5>
+            <p>____________________________</p>
+           <div class="definition"> <p>Phone Number: ${item.phone_number}</p></div>
+            <p>email: ${item.email}</p>
+          <p>${item.date}</p>
+          <p>${item.is_phone === true ? 'Phone Order' : 'In Person'}</p>
+          <p>${item.is_open === true ? 'order open' : 'order closed'}</p>
+            
+            ${item.is_open === true ? `<i id="edit-Order-btn--${item.firebaseKey}" class="logout-btn fas  btn btn-info"><i class="fas fa-edit"></i> Edit</i>` : ''}
+            ${item.is_open === true ? `<i id="details-Order-btn--${item.firebaseKey}" class="logout-btn btn btn-success fas"><i class="fas fa-eye"></i> View Details</i>` : ''}
+            ${item.is_open === true ? `<i id="delete-Order-btn--${item.firebaseKey}" class="logout-btn btn btn-danger fas"><i class="fas fa-trash-alt"></i> Delete</i>` : ''}
+
+    
+        </div>`;
+          }
+          renderToDOM('#store', domStringFilter);
         });
-      })
+      });
     }
     // CLICK EVENT FOR DELETING AN ORDER
     if (e.target.id.includes('delete-Order-btn')) {
